ff00b00c3030d9a46b2f4f65c997f9fa600942f8
Facundo caballi <azdelugano001mela@gmail.com>
2023-09-18 15:30:07 -0300

    lo arregle

--- Ejercicios_2_cuatrimestre/delete/actividad3.sql
+++ Ejercicios_2_cuatrimestre/delete/actividad3.sql
@@ -1,1 +1,4 @@
-DELETE FROM address WHERE address not in (SELECT city FROM city)
+DELETE FROM address WHERE city_id not in (SELECT address FROM address)
+/* no me funciona el primero, yo pienso que no funciona porque no hay ninguno que le falte*/
+
+DELETE FROM city WHERE country_id not in (SELECT country_id from country)
